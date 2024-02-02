5e4e07e2077b904a649b1c52a4a0650a9d0ce55c
dz1to4ka <117407093+illiaBabak@users.noreply.github.com>
2024-02-02 12:12:54 +0100

    Bulding a project

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import eslint from 'vite-plugin-eslint';
+import checker from 'vite-plugin-checker';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  server: {
+    open: true,
+    port: 3000,
+  },
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
+  plugins: [react(), eslint(), checker({ typescript: true })],
+  resolve: {
+    alias: {
+      src: '/src',
+    },
+  },
+});
