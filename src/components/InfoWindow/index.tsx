82083d1c07414b805ac1dd1739823f6f720eff85
dz1to4ka <117407093+illiaBabak@users.noreply.github.com>
2024-04-25 21:57:43 +0200

    Added ability to view more info about movie

---
+++ src/components/InfoWindow/index.tsx
@@ -0,0 +1,68 @@
+import { useContext } from 'react';
+import { GlobalContext } from 'src/root';
+import { MovieType } from 'src/types/types';
+import { formatDateToWords, roundVote } from 'src/utils/format';
+
+type Props = {
+  movie: MovieType;
+  setShouldShowInfo: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+export const InfoWindow = ({ movie, setShouldShowInfo }: Props): JSX.Element => {
+  const { genres } = useContext(GlobalContext);
+
+  return (
+    <div className='movie-info-wrapper' onClick={() => setShouldShowInfo(false)}>
+      <div
+        className='movie-info'
+        onClick={(e) => {
+          e.preventDefault();
+          e.stopPropagation();
+        }}
+      >
+        <img
+          src={`http://image.tmdb.org/t/p/original${movie.backdrop_path}`}
+          alt='background-img'
+          className='background-img'
+        />
+
+        <div className='left-col'>
+          <img className='info-poster' src={`http://image.tmdb.org/t/p/original${movie.poster_path}`} alt='movie_img' />
+        </div>
+
+        <div className='right-col'>
+          <h2>Title: {movie.original_title}</h2>
+          <p>Release date: {formatDateToWords(movie.release_date)}</p>
+          <div className='genres-list'>
+            <p>Genres: </p>
+            {movie.genre_ids.map((id, index) => {
+              const genre = genres.find((el) => el.id === id)?.name;
+
+              return (
+                <p
+                  className='movie-genre'
+                  key={`genre-${id}`}
+                >{`${genre ?? ''}${index < movie.genre_ids.length - 1 ? ',' : ''}`}</p>
+              );
+            })}
+          </div>
+
+          <div className='vote-wrapper'>
+            Vote:
+            <img className='movie-vote-img' src='https://pngfre.com/wp-content/uploads/star-png-image-pngfre-2.png' />
+            <p className='movie-vote'>{roundVote(movie.vote_average)}</p>
+          </div>
+
+          <p>Adult: {movie.adult ? 'adult' : 'not an adult'}</p>
+          <p>Original language: {movie.original_language}</p>
+          <p>Popularity: {movie.popularity}</p>
+          <p className='overview'>{movie.overview}</p>
+        </div>
+
+        <div className='close-btn' onClick={() => setShouldShowInfo(false)}>
+          x
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/components/Movie/index.tsx
+++ src/components/Movie/index.tsx
@@ -4,6 +4,7 @@ import { GlobalContext } from 'src/root';
 import { FavouritesList, MovieType } from 'src/types/types';
 import { roundVote } from 'src/utils/format';
 import { getFavourites } from 'src/utils/getFavouritesMovies';
+import { InfoWindow } from '../InfoWindow';
 
 type Props = {
   movie: MovieType;
@@ -12,6 +13,7 @@ type Props = {
 export const Movie = ({ movie }: Props): JSX.Element => {
   const { genres, currentUser } = useContext(GlobalContext);
   const [favouritesMovies, setFavouritesMovies] = useState<MovieType[]>([]);
+  const [shouldShowInfo, setShouldShowInfo] = useState(false);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -56,49 +58,57 @@ export const Movie = ({ movie }: Props): JSX.Element => {
   };
 
   return (
-    <div className='movie-card'>
-      <img src={`http://image.tmdb.org/t/p/original${movie.poster_path}`} alt='poster' className='movie-poster' />
-      <h4 className='movie-title'>{movie.original_title}</h4>
+    <>
+      <div className='movie-card'>
+        <img src={`http://image.tmdb.org/t/p/original${movie.poster_path}`} alt='poster' className='movie-poster' />
+        <h4 className='movie-title'>{movie.original_title}</h4>
 
-      <div className='card-info'>
-        <div className='card-genres'>
-          {movie.genre_ids.map((id, index) => {
-            if (index >= 2) return;
+        <div className='card-info'>
+          <div className='card-genres'>
+            {movie.genre_ids.map((id, index) => {
+              if (index >= 2) return;
 
-            const genre = genres.find((el) => el.id === id)?.name;
-            return (
-              <p
-                className='movie-genre'
-                key={`genre-${id}`}
-              >{`${genre ?? ''}${index == 0 && movie.genre_ids.length !== 1 ? ',' : ''}`}</p>
-            );
-          })}
-        </div>
-        x
-        <div className='movie-vote-row'>
-          <img className='movie-vote-img' src='https://pngfre.com/wp-content/uploads/star-png-image-pngfre-2.png' />
-          <p className='movie-vote'>{roundVote(movie.vote_average)}</p>
-          <div
-            className='add-list-btn'
-            onClick={
-              currentUser
-                ? favouritesMovies.some((fav) => fav.id === movie.id)
-                  ? () => removeFromList()
-                  : () => addMovieToList()
-                : () => navigate('/login')
-            }
-          >
-            <img
-              className='heart-icon'
-              src={
-                favouritesMovies.some((fav) => fav.id === movie.id)
-                  ? 'https://cdn-icons-png.freepik.com/256/9484/9484251.png?semt=ais_hybrid'
-                  : 'https://static-00.iconduck.com/assets.00/heart-icon-512x441-zviestnn.png'
+              const genre = genres.find((el) => el.id === id)?.name;
+              return (
+                <p
+                  className='movie-genre'
+                  key={`genre-${id}`}
+                >{`${genre ?? ''}${index == 0 && movie.genre_ids.length !== 1 ? ',' : ''}`}</p>
+              );
+            })}
+          </div>
+
+          <div className='movie-vote-row'>
+            <img className='movie-vote-img' src='https://pngfre.com/wp-content/uploads/star-png-image-pngfre-2.png' />
+            <p className='movie-vote'>{roundVote(movie.vote_average)}</p>
+            <div
+              className='add-list-btn'
+              onClick={
+                currentUser
+                  ? favouritesMovies.some((fav) => fav.id === movie.id)
+                    ? () => removeFromList()
+                    : () => addMovieToList()
+                  : () => navigate('/login')
               }
-            />
+            >
+              <img
+                className='heart-icon'
+                src={
+                  favouritesMovies.some((fav) => fav.id === movie.id)
+                    ? 'https://cdn-icons-png.freepik.com/256/9484/9484251.png?semt=ais_hybrid'
+                    : 'https://static-00.iconduck.com/assets.00/heart-icon-512x441-zviestnn.png'
+                }
+              />
+            </div>
+          </div>
+
+          <div onClick={() => setShouldShowInfo(true)} className='info-btn'>
+            Info
           </div>
         </div>
       </div>
-    </div>
+
+      {shouldShowInfo && <InfoWindow movie={movie} setShouldShowInfo={setShouldShowInfo} />}
+    </>
   );
 };
